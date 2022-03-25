bb2d07834baa8f03083983afe04465e0875b9316
mrican217@gmail.com <mrican217@gmail.com>
2022-03-25 14:39:41 -0400

    made edit to config.js

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,6 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
-      user: 'root',
-      password: 'password',
       database: 'newsfeed_db',
       dialect: 'mysql',
       port: 3306
